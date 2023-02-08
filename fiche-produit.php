9e318099f332e5276f210f634ca966aa3b838f7f
Guillaume BERTIN <80512116+Guibertinpro@users.noreply.github.com>
2023-02-08 11:32:31 +0100

    Added try catch

--- fiche-produit.php
+++ fiche-produit.php
@@ -1,22 +1,41 @@
 <?php include_once('header.php');
 
-  $productController = new ProductController;
-  $product = $productController->getById($_GET['id']);
+  try {
+    $productController = new ProductController;
+    $product = $productController->getById($_GET['id']);
+    ?>
 
-?>
+    <main class="d-flex">
+      
+      <?php include_once('sidebar.php'); ?>
 
-<main class="d-flex">
+      <section id="main" class="w-100">
+        <a href="produits.php" class="position-absolute px-2 py-1 m-2"><i class="fas fa-arrow-left"></i></a>
+        <h1 class="h1 text-center m-3">Détail du produit numéro <?= $product->getId() ?></h1>
+        <div class="card position-absolute top-50 start-50 translate-middle p-3">
+          <p class="h3">Nom : <?= $product->getName() ?></p> 
+          <p class="h4">Prix : <?= $product->getPrice() ?> €</p> 
+        </div>
+      </section>
+    </main>
+
+    <?php include_once('footer.php'); ?>
   
-  <?php include_once('sidebar.php'); ?>
+  <?php
+  } catch (\Throwable $th) {
+    ?>
+    <main class="d-flex">
+      
+      <?php include_once('sidebar.php'); ?>
 
-  <section id="main" class="w-100">
-    <a href="produits.php" class="position-absolute px-2 py-1 m-2"><i class="fas fa-arrow-left"></i></a>
-    <h1 class="h1 text-center m-3">Détail du produit numéro <?= $product->getId() ?></h1>
-    <div class="card position-absolute top-50 start-50 translate-middle p-3">
-      <p class="h3">Nom : <?= $product->getName() ?></p> 
-      <p class="h4">Prix : <?= $product->getPrice() ?> €</p> 
-    </div>
-  </section>
-</main>
+      <section id="main" class="w-100">
+        <a href="produits.php" class="position-absolute px-2 py-1 m-2"><i class="fas fa-arrow-left"></i></a>
+        <h1 class="h1 text-center m-3">Désolé le produit avec l'id <?= $_GET['id'] ?> est introuvable</h1>
+      </section>
+    </main>
 
-<?php include_once('footer.php'); ?>
+    <?php include_once('footer.php'); ?>
+  <?php
+  }
+  ?>
+
